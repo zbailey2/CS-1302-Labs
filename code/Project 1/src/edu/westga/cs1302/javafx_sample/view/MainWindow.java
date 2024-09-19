151df56aecfe9da3d520c4a91a79cc312afb0db2
Zachary Bailey <zbailey2@my.westga.edu>
2024-09-19 15:35:26 -0400

    Initialized the Project 1 to Repo
    1)Set up project file for Project 1

---
+++ code/Project 1/src/edu/westga/cs1302/javafx_sample/view/MainWindow.java
@@ -0,0 +1,10 @@
+package edu.westga.cs1302.javafx_sample.view;
+
+/** Codebehind for the Main Window of the application.
+ * 
+ * @author CS 1302
+ * @version Fall 2024
+ */
+public class MainWindow {
+
+}
