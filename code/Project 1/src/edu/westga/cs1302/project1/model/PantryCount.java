dda5cfd3c840dfcb0c86683dc33b8723165ac0eb
Zachary Bailey <zbailey2@my.westga.edu>
2024-09-25 22:52:13 -0400

    Business Logic for Counting PantryCount
    1)Implemented a separate class called PantryCount.java, functions as a basic utility class
    2)Implemented a static method called countPantry, counts the quantities of all foods in the given list and returns it
    3)Added separate testing package for the new class PantryCount.java
    4)Added testing for the countPantry() method to ensure all test cases work

---
+++ code/Project 1/src/edu/westga/cs1302/project1/model/PantryCount.java
@@ -0,0 +1,28 @@
+package edu.westga.cs1302.project1.model;
+
+import java.util.ArrayList;
+
+/**
+ * Creates and manages a pantry
+ * 
+ * @author Zachary Bailey
+ * @version Fall 2024
+ */
+public class PantryCount {
+	
+	/**
+	 * Calculates the quantity of the foods in the list
+	 * @param foodsInList the foods in list and their associated quantities
+	 * @return totalQuantity the total quantity
+	 */
+	public static int countPantry(ArrayList<Food> foodsInList) { 
+		int totalQuantity = 0;
+		if (foodsInList == null) {
+			throw new IllegalArgumentException("List should not be null");
+		}
+		for (Food food : foodsInList) {
+			totalQuantity += food.getQuantity();
+		}
+		return totalQuantity;
+	}
+}
---
+++ code/Project 1/src/edu/westga/cs1302/project1/test/pantryCount/TestCountPantry.java
@@ -0,0 +1,107 @@
+package edu.westga.cs1302.project1.test.pantryCount;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import edu.westga.cs1302.project1.model.Food;
+import edu.westga.cs1302.project1.model.PantryCount;
+
+public class TestCountPantry {
+	
+	@Test
+	public void testWithEmptyArray() {
+		ArrayList<Food> foodsInList = new ArrayList<Food>();
+		
+		int totalQuantity = PantryCount.countPantry(foodsInList);
+		
+		assertEquals(0, totalQuantity);
+	}
+	
+	@Test
+	public void testWithOneFood() {
+		ArrayList<Food> foodsInList = new ArrayList<Food>();
+		Food foodOne = new Food("Apple", "Fruit");
+		
+		foodOne.setQuantity(3);
+		foodsInList.add(foodOne);
+		
+		int totalQuantity = PantryCount.countPantry(foodsInList);
+		assertEquals(3,totalQuantity);
+	}
+	
+	@Test
+	public void testWithTwoFoods() {
+		ArrayList<Food> foodsInList = new ArrayList<Food>();
+		Food foodOne = new Food("Apple", "Fruit");
+		Food foodTwo = new Food("Broccoli", "Vegetable");
+		
+		foodTwo.setQuantity(6);
+		foodOne.setQuantity(3);
+		foodsInList.add(foodOne);
+		foodsInList.add(foodTwo);
+		
+		int totalQuantity = PantryCount.countPantry(foodsInList);
+		assertEquals(9,totalQuantity);
+	}
+	
+	@Test
+	public void testWithMultipleFoods() {
+		ArrayList<Food> foodsInList = new ArrayList<Food>();
+		Food foodOne = new Food("Apple", "Fruit");
+		Food foodTwo = new Food("Broccoli", "Vegetable");
+		Food foodThree = new Food("Chicken", "Meat");
+		
+		foodTwo.setQuantity(9);
+		foodOne.setQuantity(12);
+		foodThree.increaseFoodQuantity();
+		foodsInList.add(foodOne);
+		foodsInList.add(foodTwo);
+		foodsInList.add(foodThree);
+		
+		int totalQuantity = PantryCount.countPantry(foodsInList);
+		assertEquals(22,totalQuantity);
+	}
+	
+	@Test
+	public void testWithLargeQuantities() {
+		ArrayList<Food> foodsInList = new ArrayList<Food>();
+		Food foodOne = new Food("Apple", "Fruit");
+		Food foodTwo = new Food("Broccoli", "Vegetable");
+		Food foodThree = new Food("Chicken", "Meat");
+		
+		foodTwo.setQuantity(1000);
+		foodOne.setQuantity(1200);
+		foodThree.setQuantity(1500);
+		foodsInList.add(foodOne);
+		foodsInList.add(foodTwo);
+		foodsInList.add(foodThree);
+		
+		int totalQuantity = PantryCount.countPantry(foodsInList);
+		assertEquals(3700,totalQuantity);
+	}
+	
+	@Test
+	public void testWithNullList() {
+		assertThrows(IllegalArgumentException.class, () ->{
+			PantryCount.countPantry(null);
+		});
+	}
+	
+	@Test
+	public void testThatValueResetsWhenFooiSRemoved() {
+		ArrayList<Food> foodsInList = new ArrayList<Food>();
+		Food foodOne = new Food("Apple", "Fruit");
+	
+		foodOne.setQuantity(3);
+		foodsInList.add(foodOne);
+		int totalQuantity = PantryCount.countPantry(foodsInList);
+		foodsInList.remove(foodOne);
+		
+		totalQuantity = PantryCount.countPantry(foodsInList);
+		assertEquals(0,totalQuantity);
+	}
+}
