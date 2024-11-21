709be83cd8d38081700774212f81b0932fe50160
Zachary Bailey <zbailey2@my.westga.edu>
2024-11-20 22:37:42 -0500

    Added Menu to GUI
    1)Added a menu bar to the GUI.
    2)Added two menu items to the bar menu.
    3)Created the about menu item to show information about the project.
    4)Created the close menu item to close out of the project.
    5)Created eventHandlers in th MainWindow to handle the events for both menu items.

--- code/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -2,11 +2,15 @@ package edu.westga.cs1302.password_generator.view;
 
 import edu.westga.cs1302.password_generator.viewmodel.ViewModel;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /** Codebehind for the MainWindow of the Application.
  * 
@@ -22,6 +26,9 @@ public class MainWindow {
     @FXML private TextArea output;
     @FXML private Label errorTextLabel;
     @FXML private Button generatePasswordButton;
+    @FXML private MenuItem aboutMenuItem;
+    @FXML private MenuItem closeMenuItem;
+    @FXML private AnchorPane guiPane;
     
     private ViewModel vm;
     
@@ -42,5 +49,18 @@ public class MainWindow {
     				this.vm.generatePassword();
     			} 
     	);
+    	
+    	//Menu Items
+    	this.aboutMenuItem.setOnAction((event) -> {
+    		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    		alert.setTitle("About");
+    		alert.setHeaderText("About Password Generator");
+    		alert.setContentText("This generates passwords based on the selected options.\n\n" + "Author: Zachary Bailey\n");
+    		alert.showAndWait();
+    	});
+    	this.closeMenuItem.setOnAction((event) -> {
+    		Stage stage = (Stage) (this.guiPane).getScene().getWindow();
+    		stage.close();
+    	});
     }
 }
