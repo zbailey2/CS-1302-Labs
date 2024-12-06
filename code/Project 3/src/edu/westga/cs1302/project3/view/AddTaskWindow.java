f4117e19a220d69605efa25bac1dac7992ae6d9f
Zachary Bailey <zbailey2@my.westga.edu>
2024-12-06 01:27:03 -0500

    3b GUI to Cancel AddTaskWindow
    1)Created a new fxid for the cancel button in Scenebuilder
    2)Created a setOnAction button for the cancelButton when it is pressed
    3)Created a new fxid for the AnchorPane in Scenebuilder
    4)Implemented a method to close the AnchorPane stage whenever the button is pressed

--- code/Project 3/src/edu/westga/cs1302/project3/view/AddTaskWindow.java
+++ code/Project 3/src/edu/westga/cs1302/project3/view/AddTaskWindow.java
@@ -1,6 +1,10 @@
 package edu.westga.cs1302.project3.view;
 
 import edu.westga.cs1302.project3.viewmodel.ViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /** Codebehind for the Add Task Window of the application.
  * 
@@ -9,11 +13,18 @@ import edu.westga.cs1302.project3.viewmodel.ViewModel;
  */
 public class AddTaskWindow {
 	
+	@FXML private Button cancelButton;
+	@FXML private AnchorPane addTaskGUI;
+	
 	/**
 	 * Binds the ViewModel to the codeBehind for the AddTaskWindow
 	 * @param vm the ViewModel object
 	 */
 	public void bindToVM(ViewModel vm) {
-		//Add implementation later
+		//Binds the cancel button on the Add Task window to VM
+		this.cancelButton.setOnAction((event) -> {
+			Stage stage = (Stage) (this.addTaskGUI).getScene().getWindow();
+			stage.close();
+		});
 	}
 }
